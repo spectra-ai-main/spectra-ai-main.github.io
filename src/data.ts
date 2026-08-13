530e6d02eff121854c04746fc04bd63d9e442543
jared <jach2355@gmail.com>
2026-08-13 15:09:16 +0800

    imgs deploy update

--- src/data.ts
+++ src/data.ts
@@ -26,22 +26,22 @@ export const programsData: Program[] = [
 
 export const carouselSlidesData: CarouselSlide[] = [
   {
-    imageSrc: 'images/crossroad-chatters-fireside.jpg',
+    imageSrc: '${import.meta.env.BASE_URL}images/crossroad-chatters-fireside.jpg',
     title: 'Fireside Focus Group',
     description: 'Fireside style focus groups.'
   },
   {
-    imageSrc: 'images/paper-trail-anthropic.jpg',
+    imageSrc: '${import.meta.env.BASE_URL}images/paper-trail-anthropic.jpg',
     title: 'Paper Trail Anthropic',
     description: 'Working through research papers and blogs.'
   },
   {
-    imageSrc: 'images/paper-trail-deepseek.jpg',
+    imageSrc: '${import.meta.env.BASE_URL}images/paper-trail-deepseek.jpg',
     title: 'Paper Trail DeepSeek',
     description: 'Working through research papers and blogs.'
   },
   {
-    imageSrc: 'images/crossroad-chatters-02.jpg',
+    imageSrc: '${import.meta.env.BASE_URL}images/crossroad-chatters-02.jpg',
     title: 'Crossroad Chatters',
     description: 'Casual discussions on emerging Responsible AI topics and trends.'
   }
